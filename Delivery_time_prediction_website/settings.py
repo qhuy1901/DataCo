de4ffbc436f7a75812fa34e0d456ed2056678e2f
Tạ Quang Huy <62685126+qhuy1901@users.noreply.github.com>
2022-06-05 11:32:07 +0700

    Update settings.py

--- Delivery_time_prediction_website/settings.py
+++ Delivery_time_prediction_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*)j6t33a=*#wl60hn73*h0myw1k@b=ce$u9jg+9kv=od-ei@cn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'delivery-time-prediction-w1.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'dataco-delivery-time-pred-2.herokuapp.com']
 
 
 # Application definition
